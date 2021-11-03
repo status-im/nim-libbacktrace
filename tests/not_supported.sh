4a6092eedb08f23664a5c56472a8c4a24ad77fac
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2021-11-03 15:08:57 +0100

    debug2

--- tests/not_supported.sh
+++ tests/not_supported.sh
@@ -4,7 +4,7 @@ F="$1"
 
 [[ -f "${F}" ]] || { echo "Output file not found: ${F}. Aborting."; exit 1; }
 
-[[ $(grep -A1 "getBacktrace():" "${F}" | grep -v '^$' | wc -l) == 1 ]] || { echo "Check failed."; exit 1; }
+[[ $(grep -A1 "getBacktrace():" "${F}" | grep -v '^$' | wc -l | tr -d '[:space:]') == 1 ]] || { echo "Check failed."; exit 1; }
 
 rm -f "${F}"
 
