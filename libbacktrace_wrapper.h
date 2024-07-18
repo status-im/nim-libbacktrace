55780313446b618cd226025cd0383c4c9da4a18a
Etan Kissling <etan@status.im>
2024-07-18 17:55:27 +0200

    Pass length explicitly instead of relying on NULL terminators (#44)
    The `getDebuggingInfo` function relies on `programCounters` being a
    NULL terminated list. However, none of the usage actually adds NULL...
    
    - In the path that passes `getProgramCounters` result into
      `getDebuggingInfo`, no explicit 0 value is added to `result`.
      In practice, there happens to be a 0 there very frequently,
      but it is not guaranteed (`env MallocScribble=1`), and even
      if it is not there the implementation often continues to work
      when processing the extra garbage data, silencing the problem.
    
    - In the path from Nim `addDebuggingInfo` (`system/stacktraces.nim`),
      the `programCounters` list is constructed by Nim logic and also
      does not add a 0 value to the list. This means that even if we fix
      `getProgramCounters` to produce NULL terminated list, other usage
      is still broken, and outside the control of this library.
    
    Therefore, remove the NULL terminator logic and pass length explicitly
    while retaining any early loop exits when encountering 0 for compat.
    
    Also fix some memory leaks in error conditions.

--- libbacktrace_wrapper.h
+++ libbacktrace_wrapper.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020 Status Research & Development GmbH
+ * Copyright (c) 2019-2024 Status Research & Development GmbH
  * Licensed under either of
  *  * Apache License, version 2.0,
  *  * MIT license
@@ -25,9 +25,9 @@ char *get_backtrace_c(void) __attribute__((noinline));
 
 /*
  * The returned array needs to be freed by the caller.
- * It holds at least a zero sentinel value at the end.
  */
-uintptr_t *get_program_counters_c(int max_length, int skip) __attribute__((noinline));
+uintptr_t *get_program_counters_c(
+	int max_program_counters, int *num_program_counters, int skip) __attribute__((noinline));
 
 struct debugging_info {
 	char *filename;
@@ -39,11 +39,12 @@ struct debugging_info {
  * The returned array needs to be freed by the caller.
  * Char pointers in the returned structs need to be freed by the caller.
  */
-struct debugging_info *get_debugging_info_c(uintptr_t *program_counters, int max_length);
+struct debugging_info *get_debugging_info_c(
+	const uintptr_t *program_counters, int num_program_counters,
+	int max_debugging_infos, int *num_debugging_infos);
 
 #ifdef __cplusplus
 } // extern "C"
 #endif
 
 #endif // LIBBACKTRACE_WRAPPER_H
-
